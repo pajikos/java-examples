3cc4c1a0d2363915b963be818e781ee50eceaa60
Pavel Sklenar <pajikos@gmail.com>
2018-01-03 22:57:08 +0100

    Initial spring-security-radius-login project commit

---
+++ spring-security-radius-login/src/main/java/com/pavelsklenar/radiuslogin/RadiusLoginApplication.java
@@ -0,0 +1,12 @@
+package com.pavelsklenar.radiuslogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadiusLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadiusLoginApplication.class, args);
+	}
+}
---
+++ spring-security-radius-login/src/test/java/com/pavelsklenar/radiuslogin/RadiusLoginApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pavelsklenar.radiuslogin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RadiusLoginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
