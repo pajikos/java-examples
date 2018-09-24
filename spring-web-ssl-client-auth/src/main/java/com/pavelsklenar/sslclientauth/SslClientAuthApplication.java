1aeee16d9702477662af4537b8c1be9e706e15db
Pavel Sklenar <pajikos@gmail.com>
2018-09-24 23:26:58 +0200

    Initialized spring-web-ssl-client-auth

---
+++ spring-web-ssl-client-auth/src/main/java/com/pavelsklenar/sslclientauth/SslClientAuthApplication.java
@@ -0,0 +1,12 @@
+package com.pavelsklenar.sslclientauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SslClientAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SslClientAuthApplication.class, args);
+	}
+}
