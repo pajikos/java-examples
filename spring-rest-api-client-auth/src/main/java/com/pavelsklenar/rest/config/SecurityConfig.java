4321168412d40de7d5eba5ffb8d91ed3a65b5270
Pavel Sklenar <pajikos@gmail.com>
2018-10-07 23:52:36 +0200

    Added secured support on methods

--- spring-rest-api-client-auth/src/main/java/com/pavelsklenar/rest/config/SecurityConfig.java
+++ spring-rest-api-client-auth/src/main/java/com/pavelsklenar/rest/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.pavelsklenar.rest.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
