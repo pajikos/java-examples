aaa22f316207d898f6e22f4dd0db0d8c0bbed504
Pavel Sklenar <pajikos@gmail.com>
2018-12-06 20:20:43 +0100

    Init spring-boot-docker-build

---
+++ spring-boot-docker-build/src/main/java/com/pavelsklenar/rest/RestApiClientAuthApplication.java
@@ -0,0 +1,13 @@
+package com.pavelsklenar.rest;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiClientAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiClientAuthApplication.class, args);
+	}
+}
